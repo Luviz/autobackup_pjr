fdadb7bf2f18182b752d4329dc351340818a888f
Luviz <bardiajedi @gmail.com>
2013-07-07 19:00:30 +0200

    start

---
+++ autoback.ps1
@@ -0,0 +1,21 @@
+﻿$pName = Get-Content data.d;
+if (Test-Path $pName) {
+    echo ziping $pName ...
+    $shell_app=new-object -ComObject shell.application
+    $zName = "test.zip"
+    $zip_file = $shell_app.namespace((Get-Location).Path + "\$zName")
+    $destination = $shell_app.namespace((Get-Location).Path)
+    $destination.Copyhere($zip_file.items())  
+
+}else{
+    echo incorect path!;
+}
+
+/*
+    $shell_app=new-object -ComObject shell.application
+    $zName = "test.zip"
+    $zip_file = $shell_app.namespace((Get-Location).Path + "\$zName")
+    $destination = $shell_app.namespace((Get-Location).Path)
+    $destination.Copyhere($zip_file.items())
+
+*/
