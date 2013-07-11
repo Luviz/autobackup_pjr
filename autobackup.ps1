fe4e9e8a966ccfe9428610e9f49b20266676334f
Luviz <bardiajedi @gmail.com>
2013-07-11 17:24:45 +0200

    window ending

---
+++ autobackup.ps1
@@ -0,0 +1,15 @@
+﻿$pName = Get-Content data.d;
+$pwd_const = $pwd
+if (Test-Path $pName) {
+    $timestamp = Get-Date -Format o | foreach {$_ -replace ":", "_"}
+    $time = $timestamp.Remove($timestamp.IndexOf('.'))
+    echo $time
+    mkdir $time
+    cd $pwd_const\$time
+    echo "cpi -Recurse $pName ..."
+
+    cpi -Recurse $pName
+    cd $pwd_const
+}else{
+    echo incorect path!;
+}
